572aeeea53239e236689a559cc9d85549e71d700
Marco Tan <marco@ionicargon.ca>
2024-09-11 21:45:39 -0400

    messed up middleware oops

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest, res: NextResponse) {
-  const userAgent = req.headers.get('user-agent');
+  const userAgent = req.headers.get('User-Agent');
   const mobileAgents = new RegExp(
     'Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini'
   );
 
-  const isMobile = userAgent ? () => mobileAgents.test(userAgent) : true;
+  const isMobile = userAgent ? (() => (mobileAgents.test(userAgent)))() : true;
 
   if (isMobile) {
     return NextResponse.rewrite(new URL('/mobile-message', req.url));
